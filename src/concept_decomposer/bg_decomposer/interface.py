9d2fad7484ad33978502c313063a8067b0d7db4f
tamchamchi <nguyenduytamanh03012004@gmail.com>
2025-10-17 16:41:13 +0700

    Add IBgDecomposer interface with abstract decompose method for image inpainting

---
+++ src/concept_decomposer/bg_decomposer/interface.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import List
+import numpy as np
+
+class IBgDecomposer(ABC):
+    @abstractmethod
+    def decompose(self, image: np.ndarray, masks: List[np.ndarray]) -> np.ndarray:
+        """
+        Inpaint the given object masks in the image.
+
+        Parameters
+        ----------
+        image : np.ndarray
+            Input image (H, W, C).
+        masks : List[np.ndarray]
+            List of masks (H, W), each mask corresponds to an object to inpaint.
+
+        Returns
+        -------
+        np.ndarray
+            Image after inpainting.
+        """
+        pass
